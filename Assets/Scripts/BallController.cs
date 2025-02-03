bf76564ce306b04b1009ce3ed405c0b59f518ac8
joyfuluj <joyumejiego@yahoo.com>
2025-02-03 11:21:15 -0800

    Added forward movement for the ball

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private float force = 1f;
+    [SerializeField] private InputManager inputManager;
+
+    private Rigidbody ballRB;
+    void Start()
+    {
+        //Grabbing a reference to RigidBody
+        ballRB = GetComponent<Rigidbody>();
+
+        // Add a listener to the OnSpacePressed event.
+        // When the space key is pressed the
+        // LaunchBall method will be called.
+        inputManager.OnSpacePressed.AddListener(LaunchBall);
+    }
+
+    private void LaunchBall()
+    {
+        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
