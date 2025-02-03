6f55f8de7e224264dcc93a3a9f11a95b8837d37b
joyfuluj <joyumejiego@yahoo.com>
2025-02-03 11:53:03 -0800

    Added pins and prevented multiple launches

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,7 +4,9 @@ using UnityEngine.Events;
 public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
-    [SerializeField] private InputManager inputManager;
+
+    private bool isBallLaunched;
+    private InputManager inputManager;
 
     private Rigidbody ballRB;
     void Start()
@@ -20,6 +22,9 @@ public class BallController : MonoBehaviour
 
     private void LaunchBall()
     {
+        if(isBallLaunched) return;
+
+        isBallLaunched = true;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 
