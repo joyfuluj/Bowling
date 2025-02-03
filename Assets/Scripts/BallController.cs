aaa3e3c135939eb8a8dc4e5db179dbce117e3f06
joyfuluj <joyumejiego@yahoo.com>
2025-02-03 12:18:18 -0800

    Basic bowling and pins setup

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,7 +6,7 @@ public class BallController : MonoBehaviour
     [SerializeField] private float force = 1f;
 
     private bool isBallLaunched;
-    private InputManager inputManager;
+    [SerializeField] private InputManager inputManager;
 
     private Rigidbody ballRB;
     void Start()
